4bfc3f3ac9d27bb87f0da09dd899b5d05db34d27
Jeremy Kauffman <jeremy@blockstream.io>
2017-12-29 17:25:30 +0000

    Remove seemingly pointless code from prepare_wally.sh

--- prepare_wally.sh
+++ prepare_wally.sh
@@ -2,26 +2,7 @@
 
 set -e
 
-if [ "$(uname -s)" == "Darwin" ]; then
-    OSX=true
-fi
-
-APPDIR=$PWD
-READLINK=readlink
-if which greadlink; then
-    READLINK=greadlink
-elif [ -n "$OSX" ]; then
-    echo "greadlink missing! Try brew install coreutils."
-    exit 1
-fi
-
 cd libwally-core
-source ./tools/android_helpers.sh
-
-all_archs=$(android_get_arch_list)
-if [ -n "$1" ]; then
-    all_archs="$1"
-fi
 
 echo '============================================================'
 echo 'Initialising build:'
